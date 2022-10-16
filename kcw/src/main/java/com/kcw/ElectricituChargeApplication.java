b31b583c5ac5e9e9909c96f924fe4f7ebe53df7f
kangchaewon <erica_0809@naver.com>
2022-10-16 11:28:16 +0900

    initial commit

---
+++ kcw/src/main/java/com/kcw/ElectricituChargeApplication.java
@@ -0,0 +1,13 @@
+package com.kcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricituChargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricituChargeApplication.class, args);
+	}
+
+}
---
+++ kcw/src/test/java/com/kcw/ElectricituChargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricituChargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
